f89683d558976e1fdea7faf7fda055aea10193da
Mayuresh-Sonawane07 <mayureshsonawane1526@gmail.com>
2026-08-19 14:40:00 +0530

    Added Feedback Form

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+const feedbackList = [];
+
 app.get('/', (req, res) => {
-	res.send('DevOps CI/CD Project Working');
-	});
+    const feedbackHTML = feedbackList.map(f => `<li><strong>${f.name}:</strong> ${f.msg}</li>`).join('');
+
+    res.send(`
+        <h1>DevOps CI/CD Project Working</h1>
+        
+        <h3>Leave Feedback</h3>
+        <form action="/feedback" method="POST">
+            <input type="text" name="name" placeholder="Your Name" required /><br><br>
+            <textarea name="msg" placeholder="Your Feedback" required></textarea><br><br>
+            <button type="submit">Submit Feedback</button>
+        </form>
+
+        <h3>Recent Feedback</h3>
+        <ul>
+            ${feedbackHTML || '<li>No feedback yet. Be the first!</li>'}
+        </ul>
+    `);
+});
+
+app.post('/feedback', (req, res) => {
+    const { name, msg } = req.body;
+    if (name && msg) {
+        feedbackList.push({ name, msg });
+    }
+    res.redirect('/');
+});
+
 app.listen(3000, () => {
-	console.log('Running on port 3000');
-	});
+    console.log('Running on port 3000');
+});
